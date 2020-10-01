46514d6f76f3bbd96a65b89520dd51be3e6f9097
repl.it user <replituser@example.com>
2020-10-01 09:13:22 +0000

    user input

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+print("It is nice to meet you human", name)
+whole_number = int(input())
+decimal_number = float(input())
+print("Your age is " + str(user_age))
