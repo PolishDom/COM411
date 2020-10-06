95d54bcc753b2e845f17fda7b07e40b04ed4fac6
repl.it user <replituser@example.com>
2020-10-06 10:02:40 +0000

    Lab 2 Painting

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,19 @@
+
+paintdirection = input("What direction would you like to paint in? ")
+
+if paintdirection == "Up" or paintdirection == "up":
+  print ("PAINTING UP")
+
+elif paintdirection == "Right" or paintdirection == "right":
+  print ("PAINTING RIGHT")
+
+elif paintdirection == "Left" or paintdirection == "left":
+  print ("PAINTING LEFT")
+
+elif paintdirection == "Down" or paintdirection == "down":
+  print ("PAINTING DOWN")
+
+else:
+  print ("I DONT KNOW WHAT DIRECTION?!?!")
+
+print ("Well done on painting")
