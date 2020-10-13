f42f2cb8cc87916878f7663424b69ee492a7b477
repl.it user <replituser@example.com>
2020-10-13 10:39:46 +0000

    after save

---
+++ basics/functions/sounds.py
@@ -0,0 +1,10 @@
+def loudsound():
+  sound = input("What word would you describe the sound?")
+  print ("What did you just hear?")
+  print (sound)
+
+  print("That was a loud",sound)
+
+
+
+loudsound()
