d38e5372aff3e95dd44418099a830dffa0bfe6c0
repl.it user <replituser@example.com>
2020-10-13 10:47:01 +0000

    boulder

---
+++ basics/functions/boulder.py
@@ -0,0 +1,15 @@
+
+def boulder():
+  see = input("Can you see a large boulder or nothing?")
+
+  if see == "a large boulder":
+    print ("Its time to run!")
+  elif see == ("nothing"):
+    print("We will be fine")
+
+  else:
+    print("you have misentered the statement")
+
+
+boulder()
+print ("finish")
