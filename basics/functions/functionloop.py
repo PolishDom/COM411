4a9049d6bebe82d53c05029be4ea13ef7adc3faf
repl.it user <replituser@example.com>
2020-10-16 10:21:13 +0000

    multipleparameters

---
+++ basics/functions/functionloop.py
@@ -0,0 +1,14 @@
+def cross_bridge(steps):
+  
+  for step in range(steps):
+    print("crossed step.")
+
+
+  if (steps > 5):
+    print("The bridge is collapsing!") 
+    
+  else:
+    ("we must keep going")
+
+cross_bridge(3)
+cross_bridge(6)
---
+++ basics/functions/multipleparameters.py
@@ -0,0 +1,9 @@
+def climb_ladder (stepstogo , stepspast):
+  if (stepstogo > stepspast):
+    print ("Still some way to go!")
+
+  else:
+    print("we are almost there!")
+
+climb_ladder(5,2)
+climb_ladder(2,5)
