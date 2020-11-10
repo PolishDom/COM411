122e49c2f0b41aa3e73ad4b2a69c0a2f21b8f3bb
repl.it user <replituser@example.com>
2020-11-10 11:29:45 +0000

    Task 25 Activity 3

--- basics/plots/task25.3.py
+++ basics/plots/task25.3.py
@@ -1,1 +1,32 @@
 import matplotlib.pyplot as plt
+
+def coordinate():
+  xvalue = input("Please enter a x value")
+  yvalue = input("Please enter a Y value")
+  x_and_y_values = (xvalue , yvalue)
+  return x_and_y_values
+
+def path():
+  print ("Retrieving path...")
+  x_values = []
+  y_values = []
+  x = range(4)
+  for i in x:
+    data = coordinate()
+    x_values.append(data[0])
+    y_values.append(data[1])
+  
+  return [x_values, y_values]
+
+def run():
+  values = path()
+  plt.plot(values[0],values[1] , "ro--")
+  plt.show()
+
+run()
+
+
+
+
+
+
