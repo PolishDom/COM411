fe4fb0305189c131f4cb95d9ffe1bda7fc669d67
repl.it user <replituser@example.com>
2020-12-06 15:12:56 +0000

    grow eat and move

--- basics/class/Human.py
+++ basics/class/Human.py
@@ -12,8 +12,31 @@ class Human:
     self.energy = self.MAX_ENERGY 
 
   #instance methods
+  def grow(self):
+    self.age += 1
+
+  def eat(self, eating):
+    eatingamount = self.energy + eating
+    if (eatingamount > Robot.MAX_ENERGY):
+      self.energy = Human.MAX_ENERGY
+      return eatingamount - self.energy
+    else:
+      self.energy = eatingamount
+      return 0
+  
+  def move(self, distance):
+    eatingamount = self.energy - distance
+    if (eatingamount < 0):
+      self.energy = 0
+      return self.energy - abs(eatingamount)
+    else:
+      self.energy = eatingamount
+      return 0 
+
   def display(self):
-    print(f"I am {self.name}")
+    print(f"I am {self.name}, age {self.age} and my energy level is {self.energy}")
+
+  
 
 if (__name__ == "__main__"):
   human = Human()
--- basics/class/Robot.py
+++ basics/class/Robot.py
@@ -6,12 +6,35 @@ class Robot:
 
   #initializer
   def __init__(self):
+    
     #Instance attributes
     self.name = "Robot"
     self.age = 0
     self.energy = 0 
 
   #instance methods
+
+  def grow(self):
+    self.age += 1
+
+  def eat(self, eating):
+    eatingamount = self.energy + eating
+    if (eatingamount > Robot.MAX_ENERGY):
+      self.energy = Robot.MAX_ENERGY
+      return eatingamount - self.energy
+    else:
+      self.energy = eatingamount
+      return 0
+  
+  def move(self, distance):
+    eatingamount = self.energy - distance
+    if (eatingamount < 0):
+      self.energy = 0
+      return self.energy - abs(eatingamount)
+    else:
+      self.energy = eatingamount
+      return 0 
+
   def display(self):
     print(f"I am {self.name}, age {self.age} and my energy level is {self.energy}")
 
